843d2209138fa728cd4b8cbf4a567970122372fe
mobi phil <mobi@mobiphil.com>
2014-12-30 17:10:39 +0100

    ni

--- src/cmatch.c
+++ src/cmatch.c
@@ -105,20 +105,6 @@ static unsigned CreateTranslationUnit(CXIndex Idx, const char *file,
    return 1;
 }
 
-static int matchExpression(const char *file, const char *text, const char *selector) {
-   CXIndex Idx;
-   CXTranslationUnit TU;
-   Idx = clang_createIndex(1 , 1);
-
-   if (!CreateTranslationUnit(Idx, file, &TU)) {
-      clang_disposeIndex(Idx);
-      return 1;
-   }
-   clang_matchAst(TU, (void*)0, text, &matchCallback, (void*)selector);
-
-   return 0;
-}
-
 int specialCallback(AstNode node, void *userData) {
 
    AstNode result1, result2, result3;
@@ -133,6 +119,9 @@ int specialCallback(AstNode node, void *userData) {
    result = clang_AstNode_runMethod(node, "getParent");
    result = clang_AstNode_runMethod(result, "getNameAsString");
    result3 = clang_AstNode_runMethod(result, "toChar");
+
+
+   /* TODO implement dispose clang function and call it on objects that need to be disposed */
    printf("%s::%s -> %s\n", (const char *)result3.node, (const char*)result1.node, (const char*)result2.node);
 
    return 1;
@@ -179,12 +168,13 @@ static int special(const char *file) {
 
 int usage(const char *exename) {
    printf("usage: %s <options>\n"
-      " -a ast    --ast-file               ast files to operate on\n" 
-      " -m expr   --match-expr             ast match expression\n" 
-      " -r chain  --run                    run method chain (default is dump)\n"
-      " -C expr   --compl-match-expr       complete ast expression\n"
-      " -c expr   --compl-match-expr-det   complete ast expression with argument detail\n" 
-      " -l expr   --list-methods           list methods that can be called for a match\n" , 
+      " -a ast    --ast-file              ast files to operate on\n" 
+      " -s src    --src-file              source file to operate on (involves parsing etc)\n" 
+      " -m expr   --match-expr            ast match expression\n" 
+      " -r chain  --run                   run method chain (default is dump)\n"
+      " -C expr   --compl-match-expr      complete ast expression\n"
+      " -c expr   --compl-match-expr-det  complete ast expression with argument detail\n" 
+      " -l expr   --list-methods          list methods that can be called for a match\n" , 
       exename);
 
 }
@@ -202,9 +192,11 @@ int main(int argc, char *const*argv) {
    /* getopt_long stores the option index here. */
    int option_index = 0;
    int hasAstFile = 0;
+   int hasSrcFile = 0;
    int hasPattern = 0;
    int hasSelector = 0;
    char astFile[200];
+   char srcFile[200];
    char pattern[200];
    char selector[200];
    int compl = 0;
@@ -212,9 +204,10 @@ int main(int argc, char *const*argv) {
    int complDet = 0;
    int matching = 0;
    ASTMatchCompletionCallback cb ;
+   int argsConsumed = 1; /* bit hackish: to detect start of -- */
    while(1) {
 
-      int c = getopt_long (argc, argv, "C:c:m:a:r:l:",
+      int c = getopt_long (argc, argv, "C:c:m:a:r:l:s:",
             long_options, &option_index);
 
       /* Detect the end of the options. */
@@ -225,29 +218,57 @@ int main(int argc, char *const*argv) {
          case 'a': 
             strcpy(astFile, optarg);
             hasAstFile = 1;
+            argsConsumed +=2;
+            break;
+         case 's': 
+            strcpy(srcFile, optarg);
+            hasSrcFile = 1;
+            argsConsumed +=2;
             break;
          case 'c':
+            compl = 1;
             complDet = 1;
+            strcpy(pattern, optarg);
+            argsConsumed +=2;
+            break;
          case 'C':
             compl = 1;
             strcpy(pattern, optarg);
             hasPattern = 1;
+            argsConsumed +=2;
             break;
          case 'm':
             matching = 1;
             hasPattern = 1;
             strcpy(pattern, optarg);
+            argsConsumed +=2;
             break;
          case 'l':
             list = 1;
             hasPattern = 1;
             strcpy(pattern, optarg);
+            argsConsumed +=2;
             break;
          case 'r':
             hasSelector = 1;
             strcpy(selector, optarg);
+            argsConsumed +=2;
             break;
       }
+   }
+   
+
+   if(hasAstFile && hasSrcFile)
+   {
+      printf("either source file or ast file should be specified\n");
+      usage(argv[0]);
+      return -1;
+   }
+   if(compl && matching)
+   {
+      printf("completion or matching functionalities are exclusive\n");
+      usage(argv[0]);
+      return -1;
    }
    if(compl)
    {
@@ -258,14 +279,48 @@ int main(int argc, char *const*argv) {
          cb = cbComplDetalied;
       return completeMatchExpression(pattern, cb);
    }
-   if(matching && hasPattern && hasAstFile) {
+   if(matching && hasPattern ) {
+      CXIndex Idx;
+      CXTranslationUnit TU;
+      Idx = clang_createIndex(1 , 1);
+      
+      if(hasAstFile) {
+         if (!CreateTranslationUnit(Idx, astFile, &TU)) {
+            clang_disposeIndex(Idx);
+            return 1;
+         }
+      }
+      else if(hasSrcFile) {
+         if(argsConsumed < argc && strcmp(argv[argsConsumed], "--" )  == 0) {         
+            argsConsumed ++;
+            clang_parseTranslationUnit2(Idx, srcFile,
+                  argv + argsConsumed,
+                  argc - argsConsumed, 0,0,
+                  0, &TU);
+         }
+         else {
+            clang_parseTranslationUnit2(Idx, srcFile,
+                  0,
+                  0, 0,0,
+                  0, &TU);
+
+         }
+          
+      }
+      else  {
+         printf("either src or ast file must be specified\n");
+         usage(argv[0]);
+      }
+
       if(!hasSelector)
          strcpy(selector, "dump");
-      return matchExpression(astFile, pattern, selector);
+      clang_matchAst(TU, (void*)0, pattern, &matchCallback, (void*)selector);
    }
    if(list && hasPattern) {
       return listMethods(pattern);
    }
+
+
    usage(argv[0]);
 
    return -1;
